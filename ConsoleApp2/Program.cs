0cc05044e8ac40bb082956a83966da3e1124d6ad
Хвастунов <op.ap.191@mail.ru>
2022-10-31 11:49:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Найти периметр и длину диагонали");
+        Console.Write("a = "); int a = Int32.Parse(Console.ReadLine());
+        Console.Write("b = "); int b = Int32.Parse(Console.ReadLine());
+        int P = 2 * a + 2 * b;
+        double d = Math.Sqrt(a * a + b * b);
+        Console.Write("Периметр = {0}   Длина диагонали = {1}", P, d);
+        Console.ReadLine();
+    }
+}
